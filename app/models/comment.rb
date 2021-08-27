80286027b5cb7be87811314790bd67e8c317aebf
JosHeredia26 <jose.heredia.pachas@gmail.com>
2021-08-27 17:27:28 +0000

    add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   # Associations
   belongs_to :user
   belongs_to :tweet
+
+  # Validations
+  validates :body, presence: true
+  validates :body, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   # Associations
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  # Validations
+  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :name, presence: true
 end
